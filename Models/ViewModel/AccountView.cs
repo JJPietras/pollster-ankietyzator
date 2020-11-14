8ed0e2eacc03b3cf2bc953af1c8c6d89ef96218e
jakub_pietras <jacubeus@gmail.com>
2020-11-14 03:44:11 +0100

    Implemented Account in backend (GET PUT POST).

---
+++ Models/ViewModel/AccountView.cs
@@ -0,0 +1,12 @@
+﻿using Ankietyzator.Models.DataModel;
+
+namespace Ankietyzator.Models.ViewModel
+{
+    public class AccountView
+    {
+        public int? Id { get; set; }
+        public string UserName { get; set; }
+        public string EMail { get; set; }
+        public UserType UserType { get; set; }
+    }
+}
