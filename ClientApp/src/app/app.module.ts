d3057738f7dbf44767ab4106e87ad5f8e7b25525
pamsolik <amsithemlgpotato@gmail.com>
2020-11-16 21:25:15 +0100

    Added the settings tabs and resolved the problems with them - still need the settnigs definitions in the api

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './components/app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
@@ -12,7 +13,13 @@ import { FetchDataComponent } from './components/fetch-data/fetch-data.component
 import { LoginComponent } from './components/login/login.component'
 import { RegisterComponent } from './components/register/register.component'
 
+import { SettingsComponent } from './components/settings/settings.component'
+import { UserInfoComponent } from './components/settings/user-info/user-info.component'
 
+import {MatNativeDateModule} from '@angular/material/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import { BootstrapTabDirective } from './directives/bootstrap-tab.directive'
 
 @NgModule({
   declarations: [
@@ -21,7 +28,7 @@ import { RegisterComponent } from './components/register/register.component'
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-
+    SettingsComponent, UserInfoComponent,
     LoginComponent,
     RegisterComponent,
   ],
@@ -30,15 +37,23 @@ import { RegisterComponent } from './components/register/register.component'
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    MatTabsModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'user-login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
+      { path: 'settings', component: SettingsComponent },
     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
+
