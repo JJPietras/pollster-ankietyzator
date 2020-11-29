231558bc1eef786a48836872385f9c4642cfd071
jakub_pietras <jacubeus@gmail.com>
2020-11-29 19:34:22 +0100

    Fixed formatting.

--- Models/DataModel/AccountModel/UpgradeKey.cs
+++ Models/DataModel/AccountModel/UpgradeKey.cs
@@ -6,18 +6,13 @@ namespace Ankietyzator.Models.DataModel.AccountModel
     [Keyless]
     public class UpgradeKey
     {
-        [Required]
-        [MaxLength(200)]
-        public string Key { get; set; }
-        
-        [Required]
-        [MaxLength(200)]
-        public string EMail { get; set; }
-        
-        [Required]
-        public UserType UserType { get; set; }
+        [Required] [MaxLength(200)] public string Key { get; set; }
+
+        [Required] [MaxLength(200)] public string EMail { get; set; }
+
+        [Required] public UserType UserType { get; set; }
     }
-    
+
     public class UpgradeKeyDbContext : DbContext
     {
         public DbSet<Account> UpgradeKeys { get; set; }
