a0e64f1eecc05dfd9e01158e7cd22c43147c7c1e
jakub_pietras <jacubeus@gmail.com>
2020-11-29 19:32:40 +0100

    Fixed Answer composite key using Fluent API.

--- Models/DataModel/PollModel/Answer.cs
+++ Models/DataModel/PollModel/Answer.cs
@@ -1,23 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Ankietyzator.Models.DataModel.PollModel
 {
     public class Answer
     {
-        [Key]
+        [ForeignKey("AccountId")] 
         public int AccountId { get; set; }
-        
-        [Key]
+
+        [ForeignKey("QuestionId")] 
         public int QuestionId { get; set; }
-        
-        [Required]
-        [MaxLength(2000)]
-        public string Content { get; set; }
+
+        [Required] [MaxLength(2000)] public string Content { get; set; }
     }
-    
+
     public class AnswerDbContext : DbContext
     {
         public DbSet<Answer> Answers { get; set; }
+    }
+    
+    public class AnswerConfiguration : IEntityTypeConfiguration<Answer>
+    {
+        public void Configure(EntityTypeBuilder<Answer> builder)
+        {
+            builder.HasKey(a => new {a.AccountId, a.QuestionId});
+        }
     }
 }
