f380624422b5ce85629b4c4acde1a8f8ee9a67b6
Kademilowski <kadeb119911@gmail.com>
2021-01-02 18:32:14 +0100

    chagnes auth.service.ts

---
+++ ClientApp/src/app/models/updateDTO.model.ts
@@ -0,0 +1,6 @@
+export interface UpdateAccountDto{
+
+     EMail: string;
+     Tags: string;
+     Key: string; 
+}
--- ClientApp/src/app/services/authorisation.service.ts
+++ ClientApp/src/app/services/authorisation.service.ts
@@ -1,23 +1,34 @@
 import { Injectable, OnInit, Inject } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UpdateAccountDto } from "../models/updateDTO.model";
+import { catchError } from 'rxjs/operators';
+import Swal from "sweetalert2";
 
+//import { AngularFireAuth } from '@angular/fire/auth';
+//import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: "root",
 })
-export class AuthenticationService{
+export class AuthenticationService{ś
   private windowHandle: Window;
 
   private userSource:BehaviorSubject<User>;
   private usersSource: BehaviorSubject<User[]>;
   currentUser:Observable<User>;
   Users: Observable<User[]>;
+  //###########################
+  //do pytan ##################
+  //private questionsSource: BehaviorSubject<Question[]>;
 
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
     this.getUser()
     this.getUsers();
+
   }
+
+  
 
   get user() {
     return this.userSource;
@@ -54,10 +65,49 @@ export class AuthenticationService{
    
   }
 
+  /*const httpOptions = {
+    headers: new HttpHeaders({
+       'Content-Type': 'application/xml,application/xhtml+xml,text/html'
+    })
+  };*/
 
+  public updateUser(val: UpdateAccountDto) {
+    console.log(val);
+    //return this.http.put(this.baseUrl + 'accounts/update-my-account', val); "keys/update-key"
+     this.http.put<UpdateAccountDto>(this.baseUrl + "accounts/update-my-account", null).subscribe(
+      (result) => {
+        console.log(result);
+      },
+      (error) => {
+        Swal.fire("Błąd", error.message, "error");
+      })
+    //return this.http.put<UpdateAccountDto>(this.baseUrl + "accounts/update-my-account", val) //ze niby teraz 400 i nie moze rozpoznac zadania polcenia - 400 http
+    //return this.http.put<UpdateAccountDto>(this.baseUrl + "accounts/update-my-account", val) //wykrywa ze null i nie obsluguje czegps innego - 415 http
+          //.post<Request>("https://localhost:5001/settings/accounts/update-my-account", val)
+          
+          
+  }
+
+/*
+  this.http
+          .post<OrderRequest>(AppSettings.API_ENDPOINT + "orders", body)
+          .subscribe(
+            (result) => {
+              console.log(result);
+              Swal.fire("Gratulacje", "Dokonano zakupu pomyślnie", "info").then(
+                () => {
+                  window.location.reload();
+                }
+              );
+            },
+            (error) => {
+              Swal.fire("Błąd", error.message, "error");
+            }
+          );*/
 
   public SignInWithGoogle() {
     document.location.href = (this.baseUrl + 'google/google-login')
+    
   }
 
   public LogOutFromGoogle() {
